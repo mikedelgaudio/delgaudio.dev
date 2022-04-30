036814c3ca249409a2f97f6035f2c0315c915057
Mike DelGaudio <delgaudiomike@gmail.com>
2022-04-30 16:43:16 -0400

    adding tailwind config and cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,7 @@
 // https://vitejs.dev/config/
 import react from '@vitejs/plugin-react';
-import OpenProps from 'open-props';
-import postcssJitProps from 'postcss-jit-props';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [postcssJitProps(OpenProps)],
-    },
-  },
 });
